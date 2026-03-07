31d68efdc57c56b9e98af4c14fd69fdc093bf8aa
Ben Pirt <ben@pirt.co.uk>
2026-03-07 17:35:35 +0000

    Update the auth endpoint

--- admin/src/config.ts
+++ admin/src/config.ts
@@ -4,7 +4,7 @@ export const config = {
   githubClientId: import.meta.env.VITE_GITHUB_CLIENT_ID ?? "",
   tokenEndpoint: isDev
     ? "http://localhost:8788/token"
-    : "https://auth.repaircameras.org/token",
+    : "https://repaircameras.org/auth/token",
   redirectUri: isDev
     ? "http://localhost:5173/admin/"
     : "https://repaircameras.org/admin/",
